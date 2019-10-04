253f61d6b1669a47bf2b66e86c1790395f5d335e
pasha-nineseven <pavel@nineseven.ru>
2019-10-04 11:46:34 +0300

    comments full

--- app/js/interface.js
+++ app/js/interface.js
@@ -84,6 +84,7 @@ var divStr = 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; 
         				<li><a href="questions2.html">Вопросы2</a></li> \
         				<li><a href="questions3.html">Задать вопрос</a></li> \
         				<li><a href="logged-in.html">Вход выполнен</a></li> \
+        				<li><a href="comments.html">Комментарии</a></li> \
 					</ol> \
 				</div>';
 document.getElementsByTagName('body')[0].innerHTML += divStr;

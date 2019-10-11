994a2a55b9aca0a05df59527c788ff31bcdb9f89
pasha-nineseven <pavel@nineseven.ru>
2019-10-11 08:01:09 +0300

    +

--- app/js/interface.js
+++ app/js/interface.js
@@ -1,51 +1,3 @@
-// // Sticky Nav Component
-// var Sticky = (function() {
-//   'use strict';
-
-//   var CSS_CLASS_ACTIVE = 'is-fixed';
-
-//   var Sticky = {
-//     element: null,
-//     position: 0,
-//     addEvents: function() {
-//       window.addEventListener('scroll', this.onScroll.bind(this));
-//     },
-//     init: function(element) {
-//       this.element = element;
-//       this.addEvents();
-//       this.position = element.offsetTop ;
-//       this.onScroll();
-//     },
-//     aboveScroll: function() {
-//       return this.position < window.scrollY;
-//     },
-//     onScroll: function(event) {
-//       if (this.aboveScroll()) {
-//         this.setFixed();
-//       } else {
-//         this.setStatic();
-//       }
-//     },
-//     setFixed: function() {
-//       this.element.classList.add(CSS_CLASS_ACTIVE);
-//       // not needed if added with CSS Class
-//       this.element.style.position = 'fixed';
-//       this.element.style.top = 0;
-//     },
-//     setStatic: function() {
-//       this.element.classList.remove(CSS_CLASS_ACTIVE);
-//       // not needed if added with CSS Class
-//       this.element.style.position = 'static';
-//       this.element.style.top = 'auto';
-//     }
-//   };
-
-//   return Sticky;
-
-// })();
-
-
-
 
 function classList() {
 	// var sidebar = document.querySelector('.sticky-sidebar');
@@ -58,6 +10,7 @@ function classList() {
 	// var sticky = document.querySelector('.sticky-sidebar');
 	// if (sticky)
 	//   Sticky.init(sticky);
+	//var sticky = new Sticky('.page-aside');
 
 
 
